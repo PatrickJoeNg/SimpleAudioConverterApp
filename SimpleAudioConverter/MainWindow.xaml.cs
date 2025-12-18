efb1241a2e5d292d4b38f5a24fb99e26853de37f
Patrick Ng <itzsparty@gmail.com>
2025-12-17 22:02:02 -0500

    dialog added and safefilename

--- SimpleAudioConverter/MainWindow.xaml.cs
+++ SimpleAudioConverter/MainWindow.xaml.cs
@@ -8,7 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.IO;
+using Microsoft.Win32;
 namespace SimpleAudioConverter
 {
     /// <summary>
@@ -16,9 +17,62 @@ namespace SimpleAudioConverter
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] audioTypes = [".mp3",".flac"];
+
+        string? targetSongPath;
+        string? targetOutputPath;
         public MainWindow()
         {
             InitializeComponent();
+
+            //Check if ffmpeg is available
+            if (!File.Exists("ffmpeg.exe"))
+            {
+               MessageBox.Show("ffmpeg.exe not found. Please make sure ffmpeg is in the same directory as this application.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void FileFieldCheck()
+        {
+            if (targetSongPath == null || targetOutputPath == null)
+            {
+                MessageBox.Show("Please select a song and output path before converting.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+        }
+        private void SelectTargetSong()
+        {
+            OpenFileDialog dlg = new();
+            dlg.Filter = "Audio Files|*.flac";
+            bool? result = dlg.ShowDialog();
+
+            if(result == true)
+            {
+                SongSelectionLabel.Content = dlg.SafeFileName;
+            }
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MenuItem_Convert_Click(object sender, RoutedEventArgs e)
+        {
+            FileFieldCheck();
+        }
+
+        private void MenuItem_SelectFile_Click(object sender, RoutedEventArgs e)
+        {
+            SelectTargetSong();
+        }
+
+        private void ConvertSongBtn_Click(object sender, RoutedEventArgs e)
+        {
+            FileFieldCheck();
+        }
+        private void SelectFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SelectTargetSong();
         }
     }
 }
